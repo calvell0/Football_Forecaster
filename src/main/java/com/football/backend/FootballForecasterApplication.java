d9b44533d4ef0d6d07307fbe52c4a3079badad6a
calvell0 <143210595+calvell0@users.noreply.github.com>
2024-09-03 16:08:39 -0400

    Init

---
+++ src/main/java/com/football/backend/FootballForecasterApplication.java
@@ -0,0 +1,13 @@
+package com.football.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballForecasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballForecasterApplication.class, args);
+    }
+
+}
