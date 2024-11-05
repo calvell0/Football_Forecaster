ece0c76f710b1eaab09c294c6ba36fc1d810608e
Tsteinauer <steinauer.thomas@gmail.com>
2024-11-05 15:19:03 -0500

    current fronend changes

--- src/main/java/com/football/backend/controllers/WebController.java
+++ src/main/java/com/football/backend/controllers/WebController.java
@@ -1,41 +1,52 @@
 package com.football.backend.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.football.backend.models.Team;
-import com.football.backend.models.TempPrediction;
 import com.football.backend.repositories.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+
 @Controller
 public class WebController {
-
+    private static final Logger logger = LoggerFactory.getLogger(WebController.class);
     private final TeamRepository teamRepository;
+
     @Autowired
     public WebController(TeamRepository teamRepository) {
         this.teamRepository = teamRepository;
+
     }
 
     @GetMapping("/")
-    public String home (Model model) {
+    public String index(Model model) {
         List<Team> teams = teamRepository.findAll(Sort.by(Sort.Order.asc("id")));
+        logger.info("Teams Retrieved: {}", teams);
         model.addAttribute("teams", teams);
         return "index";
     }
 
-    @GetMapping("/prediction")
+
+
+   /* @GetMapping("/prediction")
     public String matchupPrediction(@RequestParam(name="home") int homeId, @RequestParam(name="away") int awayId, Model model) {
         Team home = teamRepository.findById(homeId);
         Team away = teamRepository.findById(awayId);
+
         model.addAttribute("homeTeam", home);
         model.addAttribute("awayTeam", away);
-        model.addAttribute("prediction", new TempPrediction(true, 0.5));
-        return "prediction";
-    }
+
+        return "prediction"; // You can modify this if you want to redirect or load another view
+    }*/
+
+
+
 }
+
+
