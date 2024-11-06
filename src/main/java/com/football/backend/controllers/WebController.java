6b5848cb859d2db00e3fefaf0ea64f5548f7a609
calvell0 <143210595+calvell0@users.noreply.github.com>
2024-11-05 19:48:00 -0500

    fix merge conflicts

--- src/main/java/com/football/backend/controllers/WebController.java
+++ src/main/java/com/football/backend/controllers/WebController.java
@@ -1,10 +1,5 @@
 package com.football.backend.controllers;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import com.football.backend.models.Team;
-
 import com.football.backend.models.NFLEvent;
 import com.football.backend.models.Team;
 import com.football.backend.models.PlaceholderPrediction;
@@ -14,64 +9,43 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-
 @Controller
 public class WebController {
-
-    private static final Logger logger = LoggerFactory.getLogger(WebController.class);
-    private final TeamRepository teamRepository;
 
     private final static Logger LOG = LoggerFactory.getLogger(WebController.class);
 
     private final TeamRepository teamRepository;
     private final ScheduleCacheManager cacheManager;
 
-
     @Autowired
     public WebController(TeamRepository teamRepository, ScheduleCacheManager cacheManager) {
         this.teamRepository = teamRepository;
-
         this.cacheManager = cacheManager;
-
     }
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String home (Model model) {
         List<Team> teams = teamRepository.findAll(Sort.by(Sort.Order.asc("id")));
-
-        logger.info("Teams Retrieved: {}", teams);
-
         List<NFLEvent> scheduledEvents = this.cacheManager.getScheduledEvents();
         LOG.info(scheduledEvents.get(0).toString());
-
         model.addAttribute("teams", teams);
         model.addAttribute("scheduledEvents", scheduledEvents);
         return "index";
     }
 
-
-
-   /* @GetMapping("/prediction")
+    @GetMapping("/prediction")
     public String matchupPrediction(@RequestParam(name="home") int homeId, @RequestParam(name="away") int awayId, Model model) {
         Team home = this.teamRepository.findById(homeId);
-        Team away = teamRepository.findById(awayId);
-
+        Team away = this.teamRepository.findById(awayId);
         model.addAttribute("homeTeam", home);
         model.addAttribute("awayTeam", away);
-
-
-        return "prediction"; // You can modify this if you want to redirect or load another view
-    }*/
-
-
-
-
         model.addAttribute("prediction", new PlaceholderPrediction(true, 0.5));
 
         return "prediction";
@@ -98,7 +72,4 @@ public class WebController {
             feed to prediction model
             return prediction object(outcome, confidence)
      */
-
 }
-
-
--- src/main/java/com/football/backend/repositories/TeamRepository.java
+++ src/main/java/com/football/backend/repositories/TeamRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Repository
 public interface TeamRepository extends ListPagingAndSortingRepository<Team, Integer> {
     List<Team> findAll(Sort sort);
+    Team findById(Integer id);
 }
 
 
