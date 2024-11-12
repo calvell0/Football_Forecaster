7dfb8fed7b0312f13ec4d602ec843a6563990916
calvell0 <143210595+calvell0@users.noreply.github.com>
2024-11-12 17:27:06 -0500

    Revert "commit"
    This reverts commit f705aad7cbb4903ff9e33380ec9df27f5bd4a5e0.

--- scripts/dist/tests/index.test.js
+++
@@ -1,8 +0,0 @@
-import { DataService } from "../services/DataService.js";
-import { MockDataClient } from "./MockDataClient.js";
-const dataService = DataService.build(new MockDataClient());
-// test("Team data populated", (t, done) => {
-//     const teams = dataService.getTeams();
-//     assert(teams.length > 0);
-//     done();
-// })
